f8f8695fc245e89457c52edcbc6066cee970b698
Mike Holzinger <mike.holzinger@gmail.com>
2021-01-25 14:21:10 -0800

    Adds samba support

--- mount_nas.sh
+++ mount_nas.sh
@@ -12,15 +12,34 @@
 # brew cask install osxfuse
 # brew install sshfs
 
-# sudo sshfs -o allow_other,defer_permissions user@host:/tmp/id/ /tmp/id
+# sudo sshfs -o allow_other,defer_permissions mikeholzinger@macmini:/tmp/id/ /tmp/id
 
 JSON_FILE=nas.json
 
-# NAS address and mount points
-mount_points=$(jq '.[] | length' "$JSON_FILE")
+# This function borrowed from:
+# https://apple.stackexchange.com/questions/697/how-can-i-mount-an-smb-share-from-the-command-line
 
-for index in $(seq 0 ${#mount_points[@]})
+function try_samba_mount
+{
+
+  # mount volume "smb://user@fqdn1/volume1"
+  # mount volume "smb://user@fqdn2/volume2"
+
+  osascript <<EOF
+mount volume "$1"
+EOF
+}
+
+
+# ----------------- NAS ----------------- #
+
+# NAS address and mount points
+nas_mount_points=$(jq '.nas | length' "$JSON_FILE")
+
+for index in $(seq 0 $nas_mount_points)
 do
+  if [[ $index -eq $nas_mount_points ]]; then continue; fi
+
   host=$(jq -r .nas[$index].host < nas.json)
   local=$(jq -r .nas[$index].local < nas.json)
   remote=$(jq -r .nas[$index].remote < nas.json)
@@ -28,4 +47,24 @@ do
   echo sudo sshfs -o \
       sshfs_debug,allow_other,defer_permissions,IdentityFile=~/.ssh/id_rsa \
       "$(whoami)"@"$host":"$remote" "$local" | sh
+
+#  echo "$index" "\"$host\"" "\"$local\"" "\"$remote\""
+done
+
+# ----------------- SMB ----------------- #
+
+# SMB address and mount points
+smb_mount_points=$(jq '.smb | length' "$JSON_FILE")
+
+for index in $(seq 0 $smb_mount_points)
+do
+  if [[ $index -eq $smb_mount_points ]]; then continue; fi
+
+  host=$(jq -r .smb[$index].host < nas.json)
+  remote=$(jq -r .smb[$index].remote < nas.json)
+
+  echo "$index" "\"$host\"" "\"$local\"" "\"$remote\""
+
+  try_samba_mount "smb://$USER@$host/$remote"
+
 done
